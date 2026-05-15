627f5c1f755cee70e8cc7c1036d7b5f6e85c354a
leon3323 <111938876+leon3323@users.noreply.github.com>
2026-05-15 16:41:22 -0700

    Update cache name to zombie-survival-v10

--- web/sw.js
+++ web/sw.js
@@ -3,7 +3,7 @@
  * Enables offline gameplay and caching
  */
 
-const CACHE_NAME = 'zombie-survival-v9';
+const CACHE_NAME = 'zombie-survival-v10';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
